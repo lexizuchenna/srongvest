ba74b02096b4969c3d221fd9ae7b43d638bdefd8
Alexander Ukwueze <lexizuchenna@users.noreply.github.com>
2022-06-27 17:07:45 +0100

    Removed Coingecko Api

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,12 +1,5 @@
-const CoinGecko = require('coingecko-api')
-
-const CoinGeckoClient = new CoinGecko()
-
 const viewHome = async (req, res) => {
-    // let data = await CoinGeckoClient.coins.all()
-    // const coinData = data.data
-    // console.log(coinData[2].market_data.current_price)
-    // console.log(coinData)
+
     res.render('public/home');
 }
 
